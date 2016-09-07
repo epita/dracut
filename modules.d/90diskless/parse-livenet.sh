ed7945613bf17a820dff55611b5826c651579aac
Gabriel Laskar <gabriel@lse.epita.fr>
2016-09-07 17:22:14 +0200

    diskless module
    Signed-off-by: Gabriel Laskar <gabriel@lse.epita.fr>

---
+++ modules.d/90diskless/parse-livenet.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# live net images - just like live images, but specified like:
+# root=live:[url-to-backing-file]
+
+[ -z "$root" ] && root=$(getarg root=)
+. /lib/url-lib.sh
+
+str_starts "$root" "live:" && liveurl="$root"
+str_starts "$liveurl" "live:" || return
+liveurl="${liveurl#live:}"
+
+# setting netroot to "livenet:..." makes "livenetroot" get run after ifup
+if get_url_handler "$liveurl" >/dev/null; then
+    info "livenet: root image at $liveurl"
+    netroot="livenet:$liveurl"
+    root="livenet" # quiet complaints from init
+    rootok=1
+    wait_for_dev -n /dev/root
+else
+    info "livenet: no url handler for $liveurl"
+fi
